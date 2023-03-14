66e7c594c25da9a2deb60cc110d21212784529f5
OddDuck <alexh@ieee.org>
2023-03-14 13:03:01 -0500

    update qr tag

--- QR_tag_generator.py
+++ QR_tag_generator.py
@@ -1,10 +1,16 @@
 import segno
 
-tag_PrusaMk3 = segno.make('URL to Prusa docs')
-tag_PrusaMk3.save('./RawTags/tag_PrusaMk3.svg', scale=10)
-tag_PrusaMk3.save('./RawTags/tag_PrusaMk3.png', scale=10)
+tag_PrusaMk3 = segno.make('https://github.com/FRC-Team8744/Workshop_Docs/blob/main/Prusa_i3_MK3S/Prusa_i3_MK3S.md')
+tag_PrusaMk3.save('./RawTags/tag_PrusaMk3.svg', scale=5)
+tag_PrusaMk3.save('./RawTags/tag_PrusaMk3.png', scale=5)
 
 with open('./QR_tags.md', 'w') as file:
     file.write('![Prusa_i3](./RawTags/tag_PrusaMk3.png)\n')
     file.write('# 3D Printer Docs\n\n')
 
+# with open('./QR_tags.rst', 'w') as file:
+#     file.write('3D Printer Docs\n')
+#     file.write('######\n\n')
+#     file.write('.. image:: ./RawTags/tag_PrusaMk3.png\n')
+#     file.write('  :width: 400\n')
+#     file.write('  :alt: Alternative text\n\n')
